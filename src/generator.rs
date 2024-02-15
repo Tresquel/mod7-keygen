6e0cc3fd5860f14707eb853f70aa6b65171dabcf
Stachu <43729305+Tresquel@users.noreply.github.com>
2024-02-15 19:45:24 +0100

    add office key generation

--- src/generator.rs
+++ src/generator.rs
@@ -7,7 +7,7 @@ pub fn cd() -> String {
     let mut random = rand::thread_rng();
 
     let mut f_segment: i32; // 111
-    
+
     // loop forever until we find a valid first segment number thingy
     loop {
         f_segment = random.gen_range(0..999);
@@ -17,7 +17,7 @@ pub fn cd() -> String {
             break;
         }
     }
-    
+
     let mut s_segment: i32; // 1111111
 
     // loop forever until we find a valid second segment thingy
@@ -26,11 +26,41 @@ pub fn cd() -> String {
         if digit_sum(s_segment) % 7 != 0 || s_segment % 10 >= 8 {
             continue;
         } else {
-            break
+            break;
         }
     }
 
     format!("{:0>3}-{:0>7}", f_segment, s_segment)
+}
+
+pub fn office() -> String {
+    let mut random = rand::thread_rng();
+
+    let mut f_segment = random.gen_range(1..999); // 1112
+    let mut last_digit = f_segment % 10;
+    if last_digit == 9 {
+        // 9 + 1 = 10 so just wrap it down to 0
+        last_digit = 0;
+    } else {
+        last_digit += 1;
+    }
+    f_segment = (f_segment.to_string() + &last_digit.to_string())
+        .parse()
+        .unwrap(); // combine the 3 digits with the other one
+
+    let mut s_segment: i32; // 1111111
+
+    // loop forever until we find a valid second segment thingy
+    loop {
+        s_segment = random.gen_range(7..9999997);
+        if digit_sum(s_segment) % 7 != 0 || s_segment % 10 >= 8 {
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    format!("{:0>4}-{:0>7}", f_segment, s_segment)
 }
 
 fn digit_sum(mut n: i32) -> i32 {
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,56 @@ use std::env;
 mod generator;
 
 fn help() {
-    println!("usage:");
+    println!(
+        "usage:
+-cd
+    Generates a CD key
+-office 
+    Generates a Office key
+    
+optional arguments:
+amount <integer>
+    For example: `-cd 15` will generate 15 CD keys
+    "
+    );
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    match args.len() { 
-        2 => {
+
+    match args.len() {
+        2 | 3 => {
+            let mut amount: i32 = 1;
+            if args.len() >= 3 {
+                amount = args[2].parse().unwrap_or(1);
+            }
+
             match args[1].to_lowercase().as_str() {
                 "-cd" => {
-                    println!("Generated CD Key: {}", generator::cd());
-                    return
+                    println!("Generated CD key(s): ");
+                    for _ in 0..amount {
+                        println!("{}", generator::cd());
+                    }
+
+                    return;
                 }
 
+                "-office" => {
+                    println!("Generated Office key(s): ");
+                    for _ in 0..amount {
+                        println!("{}", generator::office());
+                    }
+
+                    return;
+                }
+                "-help" => {
+                    help();
+                    return;
+                }
                 _ => {
                     eprintln!("Invalid argument(s)!");
                     help();
-                    return
+                    return;
                 }
             }
         }
@@ -29,5 +61,4 @@ fn main() {
             help();
         }
     }
-
 }
